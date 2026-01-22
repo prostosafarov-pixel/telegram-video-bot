a6397e80af496e583e28939b27bd0e20e520295a
prostosafarov-pixel <prostosafarov@gmail.com>
2026-01-22 15:56:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+
     app.run_polling()
 
 
